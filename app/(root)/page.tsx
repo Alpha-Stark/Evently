ab03856c62d0de4cbb27b77df1da9a6c6a017af3
MANN SAVANI <96766945+AlphaSmashers@users.noreply.github.com>
2024-01-13 13:26:02 +0530

    Minor Tweaks and Fixes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,6 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
 
 
 export default function Home() {
@@ -12,7 +15,21 @@ export default function Home() {
             <p className="p-regular-20 md:p-regular-24">
               Book and learn helpful tips from 3,168+ mentors in world-class companies with our global community.
             </p>
+            <Button size="lg" asChild className="button w-full sm:w-fit">
+              <Link href="#events">Explore Now</Link>
+            </Button>
           </div>
+          <Image src="/assets/images/hero.png" width={1000} height={1000} alt="hero"
+          className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"/>
+        </div>
+      </section>
+      <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+        <h2 className="h2-bold">
+          Trusted by <br /> Thousands of Events
+        </h2>
+        <div className="flex w-full flex-col gap-5 md-flex-row">
+          Search
+          CatagoryFilter
         </div>
       </section>
     </>
