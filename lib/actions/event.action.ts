ecaa3aa06afea4c5a6617c342262b5900f1fd125
MANN SAVANI <96766945+AlphaSmashers@users.noreply.github.com>
2024-02-04 21:11:11 +0530

    Image domain authenticated

--- lib/actions/event.action.ts
+++ lib/actions/event.action.ts
@@ -6,6 +6,13 @@ import { handleError } from "../utils"
 import User from "../database/models/user.model"
 import { connectToDatabase } from "../database"
 import Event from "../database/models/event.model"
+import Category from "../database/models/category.model"
+
+const populateEvent = async (query: any) => {
+    return query
+        .populate({ path: 'organizer', model: User, select: '_id firstName lastName' })
+        .populate({ path: 'category', model: Category, select: '_id name' })
+}
 
 export const createEvent = async ({ event, userId, path }: CreateEventParams) => {
     try {
@@ -23,6 +30,22 @@ export const createEvent = async ({ event, userId, path }: CreateEventParams) =>
         })
         return JSON.parse(JSON.stringify(newEvent));
 
+    } catch (error) {
+        handleError(error);
+    }
+}
+
+export const getEventById = async (eventId: string) => {
+    try {
+        await connectToDatabase()
+
+        // const event = await Event.findById(eventId);
+        const event = await populateEvent(Event.findById(eventId));
+
+        if (!event) {
+            throw new Error("Event not found")
+        }
+        return JSON.parse(JSON.stringify(event));
     } catch (error) {
         handleError(error);
     }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    // domains: ["utfs.io"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        port: "",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+//** Must restart the server/application after making modification in here.
