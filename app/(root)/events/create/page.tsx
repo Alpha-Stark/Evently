8752c39fcc4f39184452d8a13d88434a9e021a09
MANN SAVANI <96766945+AlphaSmashers@users.noreply.github.com>
2024-02-04 18:38:08 +0530

    Connected to submit event/create new event

--- app/(root)/events/create/page.tsx
+++ app/(root)/events/create/page.tsx
@@ -5,6 +5,8 @@ import React from "react";
 const createEvent = () => {
     const { sessionClaims } = auth();
     const userId = sessionClaims?.userId as string; //added question mark because it was showing error that "sessionClaims can be null"
+    // We must not forget to add the userId to the clerk metadata, to actually reterive it as we are doing in the above line. And to do it we need to customize the clerk session token. Docs: https://clerk.com/docs/backend-requests/making/custom-session-token => {"userId": "{{user.public_metadata.userId}}"}
+
 
     return (
         <>
