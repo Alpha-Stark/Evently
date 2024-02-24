1f6a5a291c5dd18ceb2bc4a6f13bfd32795e454f
MANN SAVANI <96766945+AlphaSmashers@users.noreply.github.com>
2024-02-24 19:28:48 +0530

    Solved Hydration Error

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -38,8 +38,7 @@ const Card = ({ event, hasOrderLink, hidePrice }: cardPropsType) => {
                 </div>
             )}
 
-            <Link
-                href={`/events/${event._id}`}
+            <div
                 className='flex min-h-[230px] flex-col gap-3 p-5 md:gap-4'
             >
                 {!hidePrice && (<div className="flex gap-2">
@@ -54,9 +53,13 @@ const Card = ({ event, hasOrderLink, hidePrice }: cardPropsType) => {
                 <p className="p-medium-16 p-medium-18 text-grey-500">
                     {formatDateTime(event.startDateTime).dateTime}
                 </p>
-                <p className="p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black">
-                    {event.title}
-                </p>
+
+                <Link href={`/events/${event._id}`}>
+                    <p className="p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black">
+                        {event.title}
+                    </p>
+                </Link>
+
                 <div className="flex-between w-full">
                     <p className="p-medium-14 md:p-medium-16 text-grey-600">
                         {event.organizer.firstName} {event.organizer.lastName}
@@ -68,7 +71,7 @@ const Card = ({ event, hasOrderLink, hidePrice }: cardPropsType) => {
                         </Link>
                     )}
                 </div>
-            </Link>
+            </div>
 
 
         </div>
