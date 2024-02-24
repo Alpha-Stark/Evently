1dd41d4e2c23056cad061f5b5396515950eec31f
MANN SAVANI <96766945+AlphaSmashers@users.noreply.github.com>
2024-02-24 20:33:11 +0530

    checkOut setting up

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,3 +1,4 @@
+import CheckoutButton from '@/components/shared/CheckoutButton';
 import Collection from '@/components/shared/Collection';
 import { getEventById, getRelatedEventsByCategory } from '@/lib/actions/event.action';
 import { formatDateTime } from '@/lib/utils';
@@ -41,6 +42,9 @@ const EventDetails = async ({ params: { id }, searchParams }: SearchParamProps) 
                                 </p>
                             </div>
                         </div>
+
+                        {/* Checkout Button */}
+                        <CheckoutButton event={event} />
 
                         <div className="flex flex-col gap-5">
                             <div className="flex gap-2 md:gap-3">
