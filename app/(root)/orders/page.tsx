5131019074c0c84b3395fd073aeb296644b3b56d
MANN SAVANI <96766945+AlphaSmashers@users.noreply.github.com>
2024-03-10 18:52:58 +0530

    orgenized spaces

--- app/(root)/orders/page.tsx
+++ app/(root)/orders/page.tsx
@@ -16,7 +16,6 @@ const Orders = async ({ searchParams }: SearchParamProps) => {
                 <h3 className="wrapper h3-bold text-center sm:text-left ">Orders</h3>
             </section>
 
-
             <section className="wrapper mt-8">
                 {/* <Search placeholder="Search buyer name..." /> */}
             </section>
